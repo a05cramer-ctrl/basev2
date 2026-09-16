a483905ad0e876c3acd5c5bc1d5ca544cb0c4b58
a05cramer-ctrl <a05cramer@gmail.com>
2026-09-16 18:53:51 +0200

    x handle

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ window.BASEMENT_CFG = {
   CA:    "CbdJFTy4BEeDq2mzhC7YsqSDvTRFnFsUpbwZBH3hpump",
   CHAIN: "solana",                // "solana" (pump.fun) | "robinhood" (Pons)
   PAD:   "pumpfun",               // "pumpfun" | "pons" | "stonkfun"
-  X:     "https://x.com/",        // your X profile URL
+  X:     "https://x.com/BASEMENTnvda",
   BUY:   "",                      // optional: override the buy link
   CHART: "",                      // optional: override the chart link
 };
